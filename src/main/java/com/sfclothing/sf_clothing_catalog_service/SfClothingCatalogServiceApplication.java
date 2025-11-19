fbf910b5cdd661562d7f509f88b372008c535b72
Senayit <Senayita.hac@gmail.com>
2025-11-18 19:48:51 -0800

    spring boot project added

---
+++ src/main/java/com/sfclothing/sf_clothing_catalog_service/SfClothingCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sfclothing.sf_clothing_catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfClothingCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfClothingCatalogServiceApplication.class, args);
+	}
+
+}
